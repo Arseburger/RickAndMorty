daab4798d561a663663e0e558f319c09c8c8bdeb
Arseburger <japaneesegena@gmail.com>
2024-07-17 12:33:12 +0300

    Initial commit

---
+++ RickAndMorty/RickAndMorty/App/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  RickAndMorty
+//
+//  Created by Александр Королёв on 17.07.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        let vc = UINavigationController(rootViewController: MainViewController())
+        window?.rootViewController = vc
+        
+        return true
+    }
+}
+
---
+++ RickAndMorty/RickAndMorty/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  RickAndMorty
+//
+//  Created by Александр Королёв on 17.07.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
